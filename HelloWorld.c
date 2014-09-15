f3be5250d2163e70dfba05a44e46349f82b6c6d5
Daniel Schuhmann <schuhmann.daniel@googlemail.com>
2014-09-16 00:33:46 +0200

    Update HelloWorld.c

--- HelloWorld.c
+++ HelloWorld.c
@@ -15,10 +15,11 @@ int main(void)
     DDRD  = 0x00;       // PORTD ist Eingang
     PORTD = 0xFF;       // Pullup-Widerstände aktivieren
     
+    uint8_t temp;       // Variable für Tasterwerte
+    
     while (1)
     {
         // Hauptprogramm in Endlosschleife
-        uint8_t temp;   // Variable für Tasterwerte
         temp  = PIND;   // Tasterwerte speichern
         PORTB = temp;   // Tasterwerte an PORTB ausgeben
     }
